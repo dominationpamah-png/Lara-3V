76414240ba2c1938626d6b7ce74b3d08fd055b32
dominationpamah-png <dominationpamah@gmail.com>
2025-11-15 06:34:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // ELAINAMD
+SESSION_ID: process.env.SESSION_ID || "ELAINAMD", // ELAINAMD
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
